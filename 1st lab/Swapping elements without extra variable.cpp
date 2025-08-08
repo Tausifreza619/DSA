148bd85437ddd96810613a65d1ca735429aaf342
Tausifreza619 <rezatausif619@gmail.com>
2025-08-09 00:07:44 +0600

    Add files via upload

---
+++ 1st lab/Swapping elements without extra variable.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter the two numbers: "<<endl;
+    cin>>a>>b;
+    cout<<"The numbers after swapping: "<<endl;
+    cout<<(a+b-a)<<" "<<(a+b-b);
+}
